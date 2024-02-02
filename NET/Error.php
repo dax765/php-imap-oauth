f5eced3eed7bc77c1a9d40045c09345e0b87f140
Damir Gavranovic <damir@oddbytes.com>
2024-02-02 22:20:27 +0100

    - inital commit

---
+++ NET/Error.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NET;
+
+class Error extends \Exception {
+	public function __construct(private string $message, private int $errNum = 0) {}
+
+	public function getMessage() {
+		return $this->message;
+	}
+
+	public function getErrnum() {
+		return $this->errnum;
+	}
+}
